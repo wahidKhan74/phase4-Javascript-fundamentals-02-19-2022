b7d298c8bbc895d8caa439aab6dbbd53d22d920b
waheed khan <wahid---@gmail.com>
2022-02-19 21:44:26 +0530

    added javascript function & types

---
+++ js/school-operations.js
@@ -0,0 +1,4 @@
+// list of students -> id name , rollno, age, PCBM, attendence
+// show a student marks and attendence by roll number.
+// show student is pass or failed.
+// calculate total marks for student.
